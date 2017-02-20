117fd87ad39bcae4c17fab10488628494e3eafcc
initsh <k.ogino.biz@gmail.com>
2017-02-20 12:43:31 +0900

    Update centos7_httpd_vhost.sh

--- centos7_httpd_vhost.sh
+++ centos7_httpd_vhost.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 
 # check env.
-if ! egrep --quiet "CentOS.*7|Red.*7" /etc/system-release
-then
-	echo "This program supports only CentOS 7 or RedHat Enterprise Linux 7."
-	exit 1
-fi
-if [ $(whoami) != "root" ]
-then
-	echo "This program needs to be executed by root user."
-	exit 1
-fi
-
-# arguments
-if [ $# -ge 1 ]
-then
-	v_fqdn=$1
-else
-	echo "$(basename $0) must have at least one argument."
-	exit 1
-fi
+[ "$(curl -Ls initsh.github.io -o /dev/null -w '%{http_code}')" -eq 200 ] || { echo "[ERROR]: $(basename $0): Can't reach initsh.github.io"; exit 1; }
+. <(curl -Ls initsh.github.io/check_centos7.sh)
+. <(curl -Ls initsh.github.io/check_root.sh)
+. <(curl -Ls initsh.github.io/check_args.sh)
 
 # install httpd
 echo '--Httpd-------------------------'
@@ -33,69 +18,48 @@ echo '--StatusHttpd-------------------'
 systemctl status httpd
 
 # varibale
+v_fqdn=$1
 v_date="$(date +%Y%m%d)"
 v_time="$(date +%H%M%S)"
 v_backup_suffix="_${v_date}_${v_time}.backup"
+
+# edit conf
 v_httpd_conf="/etc/httpd/conf/httpd.conf"
-v_docroot="$(sed -r -e '/^[ \t]*DocumentRoot/!d' -e 's/^[ \t]*DocumentRoot "(.*)"/\1/g' ${v_httpd_conf})"
-v_vhosts_fqdn_docroot="${v_docroot}/${v_fqdn}"
-v_httpd_conf_d_dir="/etc/httpd/conf.d"
-v_httpd_vhosts_conf="/etc/httpd/conf.d/httpd-vhosts-00.conf"
-v_httpd_vhosts_fqdn_conf="/etc/httpd/conf.d/httpd-vhosts-${v_fqdn}.conf"
-v_logrotate_httpd="/etc/logrotate.d/httpd"
+[ -f ${v_httpd_conf} ] || touch ${v_httpd_conf}
+cp -p ${v_httpd_conf} ${v_httpd_conf}${v_backup_suffix}
+# edit edit edit edit
+# if no diff, overwrite file.
+[ "$(diff ${v_httpd_conf} ${v_httpd_conf}${v_backup_suffix})" ] || \mv -f ${v_httpd_conf}${v_backup_suffix} ${v_httpd_conf}
+echo '--HttpdConf---------------------'
+ls -dl "${v_httpd_conf}"*
 
 # mkdir vhost rootdir
-echo '--MakeDocumentRoot--------------'
-if [ -d "${v_docroot}" ]
-then
-	mkdir -p "${v_vhosts_fqdn_docroot}"
-	ls -ld "${v_docroot}" "${v_docroot}"/*
-else
-	echo 'httpd.conf: Something wrong about DocumentRoot.'
-	exit 1
-fi
+v_vhosts_fqdn_docroot="/var/www/${v_fqdn}"
+echo '--FqdnDocumentRoot--------------'
+mkdir -p "${v_vhosts_fqdn_docroot}"
+ls -ld "${v_vhosts_fqdn_docroot}"*
+[ -d "${v_vhosts_fqdn_docroot}" ] || { echo "[ERROR]: $(basename "${v_vhosts_fqdn_docroot}"): Something wrong about DocumentRoot."; exit 1; }
 
-# edit vhosts conf
-[ -f "${v_httpd_vhosts_conf}" ] || touch "${v_httpd_vhosts_conf}"
-cp -p "${v_httpd_vhosts_conf}" "${v_httpd_vhosts_conf}${v_backup_suffix}"
-cat <<__EOD__ >"${v_httpd_vhosts_conf}"
-# Edit 20170220
-# Deny access by IPAddress
-
-## http
-#NameVirtualhost *:80
-<VirtualHost *:80>
-    ServerName any
-    <Location />
-        Order deny,allow
-        Deny from all
-    </Location>
-</VirtualHost>
-
-## https
-#NameVirtualhost *:443
-<VirtualHost *:443>
-    ServerName any
-    <Location />
-        Order deny,allow
-        Deny from all
-    </Location>
-</VirtualHost>
-__EOD__
-[ "$(diff "${v_httpd_vhosts_conf}" "${v_httpd_vhosts_conf}${v_backup_suffix}")" ] || \mv -f "${v_httpd_vhosts_conf}${v_backup_suffix}" "${v_httpd_vhosts_conf}"
-echo '--EditHttpdVhostsConf-----------'
-cat "${v_httpd_vhosts_conf}"
-echo '--------------------------------'
-ls -l "${v_httpd_vhosts_conf}"*
+# gen fqdn crt & key
+v_fqdn_key=/etc/pki/tls/certs/${v_fqdn}.key
+v_fqdn_csr=/etc/pki/tls/certs/${v_fqdn}.csr
+v_fqdn_crt=/etc/pki/tls/certs/${v_fqdn}.crt
+echo '--GenerateSslKeys---------------'
+openssl genrsa 2048 >${v_fqdn_key}
+openssl req -new -key ${v_fqdn_key} -subj "/C=JP/CN=${v_fqdn}" >${v_fqdn_csr}
+openssl x509 -days 3650 -req -signkey ${v_fqdn_key} <${v_fqdn_csr} >${v_fqdn_crt}
 
 # edit fqdn conf
+v_httpd_conf_d_dir="/etc/httpd/conf.d"
+v_httpd_vhosts_fqdn_conf="${v_httpd_conf_d_dir}/httpd-vhosts-${v_fqdn}.conf"
+[ -d "${v_httpd_conf_d_dir}" ] || { echo "[ERROR]: $(basename ${v_httpd_conf_d_dir}): Something wrong about Directory."; exit 1; }
 [ -f "${v_httpd_vhosts_fqdn_conf}" ] || touch "${v_httpd_vhosts_fqdn_conf}"
 cp -p "${v_httpd_vhosts_fqdn_conf}" "${v_httpd_vhosts_fqdn_conf}${v_backup_suffix}"
 cat <<__EOD__ >"${v_httpd_vhosts_fqdn_conf}"
 # Edit 20170220
 
 ## http
-#NameVirtualhost *:80
+NameVirtualhost *:80
 <VirtualHost *:80>
     ServerName ${v_fqdn}
     DocumentRoot ${v_vhosts_fqdn_docroot}
@@ -105,37 +69,35 @@ cat <<__EOD__ >"${v_httpd_vhosts_fqdn_conf}"
 </VirtualHost>
 
 ## https
-#NameVirtualhost *:443
-<VirtualHost ${v_fqdn}:443>
+NameVirtualhost *:443
+<VirtualHost *:443>
     ServerName ${v_fqdn}
     DocumentRoot ${v_vhosts_fqdn_docroot}
 
     SSLEngine on
-    SSLCertificateFile /etc/pki/tls/certs/localhost.crt
-    SSLCertificateKeyFile /etc/pki/tls/private/localhost.key
+    SSLCertificateFile ${v_fqdn_crt}
+    SSLCertificateKeyFile ${v_fqdn_key}
 
     ErrorLog logs/${v_fqdn}-error_log
     CustomLog logs/${v_fqdn}-access_log combined
 </VirtualHost>
 __EOD__
 [ "$(diff "${v_httpd_vhosts_fqdn_conf}" "${v_httpd_vhosts_fqdn_conf}${v_backup_suffix}")" ] || \mv -f "${v_httpd_vhosts_fqdn_conf}${v_backup_suffix}" "${v_httpd_vhosts_fqdn_conf}"
-echo '--EditHttpdVhostsFqdnConf-----------'
-cat "${v_httpd_vhosts_fqdn_conf}"
-echo '--------------------------------'
+echo '--HttpdVhostsFqdnConf-----------'
 ls -l "${v_httpd_vhosts_fqdn_conf}"*
 
-# edit conf
-[ -f ${v_httpd_conf} ] || touch ${v_httpd_conf}
-cp -p ${v_httpd_conf} ${v_httpd_conf}${v_backup_suffix}
-# edit edit edit edit
-# if no diff, overwrite file.
-[ "$(diff ${v_httpd_conf} ${v_httpd_conf}${v_backup_suffix})" ] || \mv -f ${v_httpd_conf}${v_backup_suffix} ${v_httpd_conf}
+# check conf error
 echo '--HttpdConfError----------------'
-httpd -S >/dev/null
-echo '--------------------------------'
-ls -l "${v_httpd_conf}"*
+v_judge="$(httpd -S >/dev/null 2>/dev/stdout)"
+if [ "${v_judge}" ]
+then
+	echo "${v_judge}"
+else
+	echo "Nothing."
+fi
 
 # Edit logrotate.d/httpd
+v_logrotate_httpd="/etc/logrotate.d/httpd"
 cat <<'__EOD__' >${v_logrotate_httpd}
 #/var/log/httpd/*log {
 #    missingok
@@ -160,10 +122,8 @@ cat <<'__EOD__' >${v_logrotate_httpd}
     endscript
 }
 __EOD__
-echo '--EditLogrotate.d/httpd---------'
-cat ${v_logrotate_httpd}
-echo '--------------------------------'
-ls -l "${v_logrotate_httpd}"*
+echo '--logrotate.d/httpd-------------'
+ls -dl "${v_logrotate_httpd}"*
 
 
 #EOF
