5d2a91040a593d05f4e0137188cc6b0ebdecd720
initsh <k.ogino.biz@gmail.com>
2017-02-21 15:11:34 +0900

    Create centos6_selinux.sh

---
+++ centos6_selinux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+v_date="$(date +%Y%m%d)"
+v_time="$(date +%H%M%S)"
+v_backup_suffix="_${v_date}_${v_time}.backup"
+v_selinux_conf="/etc/selinux/config"
+
+# setenforce
+setenforce 0
+
+\cp -p "${v_selinux_conf}" "${v_selinux_conf}${v_backup_suffix:?}"
+sed -r -e 's/^[ \t]*(SELINUX=[^d].+)/#\1\n# '"$(date +'Edit %Y%m%d')"'\nSELINUX=disabled/g' "${v_selinux_conf}" -i
+[ "$(diff "${v_selinux_conf}" "${v_selinux_conf}${v_backup_suffix}")" ] || \mv -f "${v_selinux_conf}${v_backup_suffix}" "${v_selinux_conf}"
