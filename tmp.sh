78e246d92f32a147792feea8ea5c7e3de79a4ffd
initsh <k.ogino.biz@gmail.com>
2017-02-21 15:59:52 +0900

    Update tmp.sh

--- tmp.sh
+++ tmp.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+bash <(curl -Ls initsh.github.io/centos6.selinux.sh)
+bash <(curl -Ls initsh.github.io/centos7.firewalld.sh)
+bash <(curl -Ls initsh.github.io/centos7.utils.sh)
 bash <(curl -Ls initsh.github.io/centos7.httpd.vhost.sh) www.example.com
 systemctl start httpd
