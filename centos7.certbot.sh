2bd266475a9467f64044cbfd3842069ee2b6fd1e
initsh <k.ogino.biz@gmail.com>
2017-02-21 18:18:42 +0900

    Update centos7.certbot.sh

--- centos7.certbot.sh
+++ centos7.certbot.sh
@@ -4,7 +4,6 @@
 [ "$(curl -Ls initsh.github.io -o /dev/null -w '%{http_code}')" -eq 200 ] || { echo "[ERROR]: $(basename $0): Can't reach initsh.github.io"; exit 1; }
 . <(curl -Ls initsh.github.io/check.centos7.sh)
 . <(curl -Ls initsh.github.io/check.root.sh)
-. <(curl -Ls initsh.github.io/check.args.sh)
 
 # install epel-release
 . <(curl -Ls initsh.github.io/centos7.epel.sh)
@@ -19,3 +18,4 @@ echo 'EMAIL=mail@www.example.com'
 echo 'WEBROOT=/var/www/www.example.com'
 echo 'FQDN=www.example.com'
 echo 'certbot certonly --webroot --email "${EMAIL}" -w "${WEBROOT}" -d "${FQDN}"'
+echo 'ls -dl /etc/letsencrypt/live/*'
