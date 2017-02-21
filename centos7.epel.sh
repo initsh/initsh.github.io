4c7d3da7186842b130332d9b3949881e56e676ce
initsh <k.ogino.biz@gmail.com>
2017-02-21 17:34:00 +0900

    Update centos7.epel.sh

--- centos7.epel.sh
+++ centos7.epel.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Edit 20170221
 #v_epel_dir='http://dl.fedoraproject.org/pub/epel/7/x86_64/e/'
 #v_epel_url="${v_epel_dir}$(curl -kLRs $v_epel_dir | sed -r -e '/epel-release/!d' -e 's/^.*href="(epel-release.*\.rpm)".*$/\1/g')"
 v_epel_url='https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm'
