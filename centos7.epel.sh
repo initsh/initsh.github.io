3df88d1ac31979ecca4e53f8892aecc90d0d28ae
initsh <k.ogino.biz@gmail.com>
2017-02-21 17:28:19 +0900

    Update centos7.epel.sh

--- centos7.epel.sh
+++ centos7.epel.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-v_epel_dir='http://dl.fedoraproject.org/pub/epel/7/x86_64/e/'
-v_epel_url="${v_epel_dir}$(curl -kLRs $v_epel_dir | sed -r -e '/epel-release/!d' -e 's/^.*href="(epel-release.*\.rpm)".*$/\1/g')"
+#v_epel_dir='http://dl.fedoraproject.org/pub/epel/7/x86_64/e/'
+#v_epel_url="${v_epel_dir}$(curl -kLRs $v_epel_dir | sed -r -e '/epel-release/!d' -e 's/^.*href="(epel-release.*\.rpm)".*$/\1/g')"
+v_epel_url='https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm'
 yum -y install yum-utils "${v_epel_url}"
 yum-config-manager --disable epel*
