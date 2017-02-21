7311485a9684f5a9b074bf8c6a35299768f892c5
initsh <k.ogino.biz@gmail.com>
2017-02-22 03:25:06 +0900

    Update centos7.utils.sh

--- centos7.utils.sh
+++ centos7.utils.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-yum -y install yum-utils expect bind-utils traceroute nmap tcpdump mailx lsof parted openssh-clients telnet wget zip unzip vim-enhanced
+yum -y install yum-utils expect bind-utils traceroute nmap nc tcpdump mailx lsof parted openssh-clients telnet wget zip unzip vim-enhanced
