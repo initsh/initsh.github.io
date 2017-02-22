1242b9c62e9cf088db4932dc11e57472d524fd16
initsh <k.ogino.biz@gmail.com>
2017-02-22 18:46:36 +0900

    Update centos7.certbot.sh

--- centos7.certbot.sh
+++ centos7.certbot.sh
@@ -5,9 +5,14 @@
 . <(curl -LRs initsh.github.io/check.centos7.sh)
 . <(curl -LRs initsh.github.io/check.root.sh)
 
-# install jq epel-release
+# install jq epel-release net-tools
+--InstallDependence-------------------'
 . <(curl -LRs initsh.github.io/centos7.jq.sh)
 . <(curl -LRs initsh.github.io/centos7.epel.sh)
+if ! rpm --quiet -q net-tools
+then
+	yum -y install net-tools
+fi
 
 # install certbot
 echo '--InstallCertbot----------------'
@@ -28,24 +33,25 @@ fi
 # Let's Encrypt!
 if [ -n "$(echo "$1" | egrep '[^@]+@[^@\.]+\.[^@\.]+')" ] \
 && [ -n "$(echo "$2" | egrep '[^\.]+\.[^\.]+')" ] \
-&& [ -d "$3" ] 
+&& [ -n "$(netstat -lntp | awk '$0=$4' | egrep '443$')" ]
 then
 	echo '--Parameters--------------------'
 	echo '{"v_fqdn": "'"$1"'", "v_webroot_dir": "'"$2"'", "v_email_addr": "'"$3"'"}' | jq .
 	echo '--LetsEncrypt-------------------'
 	v_email_addr="$1"
 	v_fqdn="$2"
-	v_webroot_dir="$3"
-#	certbot certonly --non-interactive --agree-tos --email "${v_email_addr}" -d "${v_fqdn}" --webroot -w "${v_webroot_dir}"
 	certbot certonly --non-interactive --agree-tos --email "${v_email_addr}" -d "${v_fqdn}" --standalone-supported-challenges tls-sni-01
 	echo '--Certificate-------------------'
 	ls -dl "/etc/letsencrypt/live/${v_fqdn}/"*
 else
+	v_web_server="$(netstat -lntp | awk '{print $7,$4}' | egrep '443$' | sed -r -e 's/[0-9]+\/(.+) .+/\1/g')"
 	echo '--LetsEncrypt-------------------'
 	echo 'EMAIL="example@email.addr"'
 	echo 'WEBROOT="/var/www/www.example.com"'
 	echo 'FQDN="www.example.com"'
-	echo 'certbot certonly --non-interactive --agree-tos --email "${v_email_addr} -d "${FQDN}" --webroot -w "${WEBROOT}"'
+	echo "systemctl stop ${v_web_server}"
+	echo 'certbot certonly --non-interactive --agree-tos --email "${v_email_addr} -d "${FQDN}" --standalone-supported-challenges tls-sni-01'
+	echo "systemctl start ${v_web_server}"
 	echo 'ls -dl "/etc/letsencrypt/live/${FQDN}/"*'
 fi
 
