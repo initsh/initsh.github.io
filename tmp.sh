0c4cd22f1a7e74ad57afc7790368f47dade02850
initsh <k.ogino.biz@gmail.com>
2017-02-22 11:55:03 +0900

    Delete tmp.sh

--- tmp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-bash <(curl -Ls initsh.github.io/centos6.selinux.sh)
-bash <(curl -Ls initsh.github.io/centos7.firewalld.sh)
-#bash <(curl -Ls initsh.github.io/centos7.utils.sh)
-bash <(curl -Ls initsh.github.io/centos7.httpd.vhost.sh) a1.example.com
-bash <(curl -Ls initsh.github.io/centos7.httpd.vhost.sh) a2.example.com
-systemctl start httpd
