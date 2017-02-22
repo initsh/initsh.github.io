71ebd7b83166c59236fc6ae9ede8e67c0b62f07d
initsh <k.ogino.biz@gmail.com>
2017-02-22 11:49:18 +0900

    Update centos6.epel.sh

--- centos6.epel.sh
+++ centos6.epel.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 v_epel_dir='http://dl.fedoraproject.org/pub/epel/6/x86_64/'
-v_epel_url="${v_epel_dir}$(curl -kLRs $v_epel_dir | sed -r -e '/epel-release/!d' -e 's/^.*href="(epel-release.*\.rpm)".*$/\1/g')"
+v_epel_url="${v_epel_dir}$(curl -LRs $v_epel_dir | sed -r -e '/epel-release/!d' -e 's/^.*href="(epel-release.*\.rpm)".*$/\1/g')"
 yum -y install yum-utils "${v_epel_url}"
 yum-config-manager --disable epel*
+
+#EOF
