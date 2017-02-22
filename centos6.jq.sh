ded34766b07a9e2357c7416aa3affdc80d28447d
initsh <k.ogino.biz@gmail.com>
2017-02-22 15:00:38 +0900

    Update centos6.jq.sh

--- centos6.jq.sh
+++ centos6.jq.sh
@@ -5,8 +5,8 @@ if [ "$(jq --help >/dev/null 2>&1; echo $?)" -eq 127 ]
 then
 	curl -LR https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -o /tmp/jq
 	chmod 755 /tmp/jq
-	\mv -f /tmp/jq /usr/local/bin
-	ls -dl /usr/local/bin/jq*
+	\mv -f /tmp/jq /usr/bin
+	ls -dl /usr/bin/jq*
 fi
 
 #EOF
