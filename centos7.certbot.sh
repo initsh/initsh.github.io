ed4c011f9284d8eba2584fb5aaf73b89bcdd1674
initsh <k.ogino.biz@gmail.com>
2017-02-22 11:53:14 +0900

    Update centos7.certbot.sh

--- centos7.certbot.sh
+++ centos7.certbot.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # check env.
-[ "$(curl -Ls initsh.github.io -o /dev/null -w '%{http_code}')" -eq 200 ] || { echo "[ERROR]: $(basename $0): Can't reach initsh.github.io"; exit 1; }
-. <(curl -Ls initsh.github.io/check.centos7.sh)
-. <(curl -Ls initsh.github.io/check.root.sh)
+[ "$(curl -LRs initsh.github.io -o /dev/null -w '%{http_code}')" -eq 200 ] || { echo "[ERROR]: $(basename $0): Can't reach initsh.github.io"; exit 1; }
+. <(curl -LRs initsh.github.io/check.centos7.sh)
+. <(curl -LRs initsh.github.io/check.root.sh)
 
 # install epel-release
-. <(curl -Ls initsh.github.io/centos7.epel.sh)
+. <(curl -LRs initsh.github.io/centos7.epel.sh)
 
 # install certbot
 echo '--InstallCertbot----------------'
