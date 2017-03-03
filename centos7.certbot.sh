953e1185f61206c291aaff5b18fcf10b132cb850
initsh <k.ogino.biz@gmail.com>
2017-03-04 03:08:54 +0900

    Update centos7.certbot.sh

--- centos7.certbot.sh
+++ centos7.certbot.sh
@@ -55,7 +55,7 @@ then
 else
 	v_web_server="$(ss -lntp | awk '{print $6,$4}' | egrep '443$' | sed -r -e 's/users:\(\("([^"]*)".*/\1/g')"
 	echo "# systemctl stop ${v_web_server}" >/dev/stderr
-	echo "# certbot certonly --non-interactive --agree-tos --email ${v_email_addr} -d ${v_fqdn} --standalone-supported-challenges tls-sni-01" >/dev/stderr
+	echo "# certbot certonly --non-interactive --agree-tos --email ${v_email_addr} -d ${v_fqdn} --preferred-challenges tls-sni-01" >/dev/stderr
 	echo "# systemctl start ${v_web_server}" >/dev/stderr
 	echo "# ls -dl /etc/letsencrypt/live/${v_fqdn}/*" >/dev/stderr
 fi
