bfbcb354acbd8fc3e15cc51b02020d86e3b9e4be
initsh <k.ogino.biz@gmail.com>
2017-03-04 00:46:26 +0900

    Update centos6.awscli.sh

--- centos6.awscli.sh
+++ centos6.awscli.sh
@@ -3,13 +3,15 @@
 # pip
 if [ "$(pip --help >/dev/null 2>&1; echo $?)" -eq 127 ]
 then
-	python <(curl -Ls "https://bootstrap.pypa.io/get-pip.py")
+	echo 'python get-pip.py' | awk '{print "'$(date -Is)' "$0}' >>${HOME}/initsh.log
+	python <(curl -Ls "https://bootstrap.pypa.io/get-pip.py") | awk '{print "'$(date -Is)' "$0}' >>${HOME}/initsh.log
 fi
 
 # awscli
 if [ "$(aws --help >/dev/null 2>&1; echo $?)" -eq 127 ]
 then
-	pip install awscli
+	echo 'pip install awscli' | awk '{print "'$(date -Is)' "$0}' >>${HOME}/initsh.log
+	pip install awscli | awk '{print "'$(date -Is)' "$0}' >>${HOME}/initsh.log
 fi
 
 #EOF
