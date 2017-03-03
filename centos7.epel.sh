77c99143341332030ae586a62d8ce189c33cfa00
initsh <k.ogino.biz@gmail.com>
2017-03-04 01:01:48 +0900

    Update centos7.epel.sh

--- centos7.epel.sh
+++ centos7.epel.sh
@@ -3,26 +3,22 @@
 . <(curl -LRs initsh.github.io/check.centos7.sh)
 . <(curl -LRs initsh.github.io/check.root.sh)
 
+# functions
+. <(curl -LRs initsh.github.io/functions.sh)
+
 # variable
 v_epel_url='https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm'
 
 # install yum-utils
-echo '--InstallYumUtils---------------'
-if ! rpm -q yum-utils
-then
-	yum -y install yum-utils
-fi
+echo '# yum -y install yum-utils' | StdoutLog
+yum -y install yum-utils | StdoutLog
 
 # install epel-release
-echo '--InstallEpelRepo---------------'
-if ! rpm -q epel-release
-then
-	yum -y install "${v_epel_url}"
-fi
+echo "# yum -y install ${v_epel_url}" | StdoutLog
+yum -y install "${v_epel_url}" | StdoutLog
 
 # disable default-enable
-echo '--DisableRepoAsDefault----------'
-yum-config-manager --disable epel* >/dev/null 2>&1
-ls -ld /etc/yum.repos.d/epel*
+echo '# yum-config-manager --disable epel*' | StdoutLog
+yum-config-manager --disable epel* | StdoutLog
 
 #EOF
