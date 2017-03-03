2cd44ad7cbd50b2e0a499d3ddf870e220e9593a4
initsh <k.ogino.biz@gmail.com>
2017-03-04 02:28:50 +0900

    Update centos7.certbot.sh

--- centos7.certbot.sh
+++ centos7.certbot.sh
@@ -43,9 +43,9 @@ fi
 # vars
 v_email_addr="$1"
 v_fqdn="$2"
-
 echo '{"v_fqdn": "'"$v_email_addr"'", "v_email_addr": "'"$v_fqdn"'"}' | jq . | StdoutLog
 
+# install cert
 if [ -z "$(ss -lntp | awk '$0=$4' | egrep '443$')" ]
 then
 	echo 'certbot certonly --non-interactive --agree-tos --email "${v_email_addr}" -d "${v_fqdn}" --standalone-supported-challenges tls-sni-01' | StdoutLog
