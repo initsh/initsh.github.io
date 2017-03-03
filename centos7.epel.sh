7cfecd0c3e4846538105d7ac73bba64a6d3ea906
initsh <k.ogino.biz@gmail.com>
2017-03-04 03:46:31 +0900

    Update centos7.epel.sh

--- centos7.epel.sh
+++ centos7.epel.sh
@@ -10,14 +10,14 @@
 v_epel_url='https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm'
 
 # install yum-utils
-if ! rpm -q yum-utils
+if ! rpm -q yum-utils | StdoutLog
 then
 	echo '# yum -y install yum-utils' | StdoutLog
 	yum -y install yum-utils | StdoutLog
 fi
 
 # install epel-release
-if ! rpm -q epel-release
+if ! rpm -q epel-release | StdoutLog
 then
 	echo "# yum -y install ${v_epel_url}" | StdoutLog
 	yum -y install "${v_epel_url}" 2>/dev/stdout | StdoutLog
