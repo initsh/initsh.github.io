2b993d1359dfd97d0489f2253421dc2333fd1d88
initsh <k.ogino.biz@gmail.com>
2017-03-04 00:26:20 +0900

    Update centos7.utils.sh

--- centos7.utils.sh
+++ centos7.utils.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+. <(curl -LRs initsh.github.io/check.centos7.sh)
+. <(curl -LRs initsh.github.io/check.root.sh)
+
 yum -y install yum-utils expect net-tools bind-utils traceroute nmap nc tcpdump mailx lsof parted openssh-clients telnet wget zip unzip vim-enhanced
 
 #EOF
