754fbf08a322344f743190444490a652df8fb913
initsh <k.ogino.biz@gmail.com>
2017-03-04 03:43:11 +0900

    Update centos7.epel.sh

--- centos7.epel.sh
+++ centos7.epel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. <(curl -LRs initsh.github.io/check.centos7.sh)
+. <(curl -LRs initsh.github.io/check.centos6.sh)
 . <(curl -LRs initsh.github.io/check.root.sh)
 
 # functions
@@ -10,15 +10,19 @@
 v_epel_url='https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm'
 
 # install yum-utils
-echo '# yum -y install yum-utils' | StdoutLog
-yum -y install yum-utils | StdoutLog
+if ! rpm -q yum-utils
+then
+	echo '# yum -y install yum-utils' | StdoutLog
+	yum -y install yum-utils | StdoutLog
+fi
 
 # install epel-release
-echo "# yum -y install ${v_epel_url}" | StdoutLog
-yum -y install "${v_epel_url}" 2>/dev/stdout | StdoutLog
-
-# disable default-enable
-echo '# yum-config-manager --disable epel*' | StdoutLog
-yum-config-manager --disable epel* | StdoutLog
+if ! rpm -q epel-release
+then
+	echo "# yum -y install ${v_epel_url}" | StdoutLog
+	yum -y install "${v_epel_url}" 2>/dev/stdout | StdoutLog
+	echo '# yum-config-manager --disable epel*' | StdoutLog
+	yum-config-manager --disable epel* | StdoutLog
+fi
 
 #EOF
