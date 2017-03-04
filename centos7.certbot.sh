bac68abd9670438d38128096802503a5c7c3d57f
initsh <k.ogino.biz@gmail.com>
2017-03-04 16:29:13 +0900

    Update centos7.certbot.sh

--- centos7.certbot.sh
+++ centos7.certbot.sh
@@ -73,9 +73,10 @@ interact
 	ls -dl "/etc/letsencrypt/live/${v_fqdn}/"* >/dev/stderr
 else
 	v_web_server="$(ss -lntp | awk '{print $6,$4}' | egrep '443$' | sed -r -e 's/users:\(\("([^"]*)".*/\1/g')"
-	echo "# systemctl stop ${v_web_server}" >/dev/stderr
-	echo "# certbot certonly --agree-tos --email ${v_email_addr} -d ${v_fqdn} --preferred-challenges tls-sni-01" >/dev/stderr
-	echo "# systemctl start ${v_web_server}" >/dev/stderr
+	#echo "# systemctl stop ${v_web_server}" >/dev/stderr
+	#echo "# certbot certonly --agree-tos --email ${v_email_addr} -d ${v_fqdn} --preferred-challenges tls-sni-01" >/dev/stderr
+	#echo "# systemctl start ${v_web_server}" >/dev/stderr
+	echo "# certbot certonly --agree-tos --email ${v_email_addr} -d ${v_fqdn} --preferred-challenges tls-sni-01 --pre-hook \"systemctl stop ${v_web_server}\" --post-hook \"systemctl start ${v_web_server}\"" >/dev/stderr
 	echo "# ls -dl /etc/letsencrypt/live/${v_fqdn}/*" >/dev/stderr
 fi
 
