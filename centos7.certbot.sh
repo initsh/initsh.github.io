f0a42c7e7983cbb03bb262caacaca5d6fd176f9a
initsh <k.ogino.biz@gmail.com>
2017-03-04 12:57:26 +0900

    Update centos7.certbot.sh

--- centos7.certbot.sh
+++ centos7.certbot.sh
@@ -48,6 +48,21 @@ echo '{"v_fqdn": "'"$v_email_addr"'", "v_email_addr": "'"$v_fqdn"'"}' | jq . | S
 # install cert
 if [ -z "$(ss -lntp | awk '$0=$4' | egrep '443$')" ]
 then
+	v_expect_num="$(expect -c "
+set timeout 10
+spawn certbot certonly --agree-tos --email "${v_email_addr}" -d "${v_fqdn}" --preferred-challenges tls-sni-01
+expect \"(press 'c' to cancel):\"
+send \"c\n\"
+" | awk -F: '/standalone/{print $1}')"
+	expect -c "
+set timeout 10
+spawn certbot certonly --agree-tos --email "${v_email_addr}" -d "${v_fqdn}" --preferred-challenges tls-sni-01
+expect \"(press 'c' to cancel): \"
+send \"${v_expect_num}\n\"
+expect \"(press 'c' to cancel): \"
+send \"c\n\"
+interact
+"
 	echo "# certbot certonly --agree-tos --email ${v_email_addr} -d ${v_fqdn} --preferred-challenges tls-sni-01" >/dev/stderr
 	echo "# ls -dl /etc/letsencrypt/live/${v_fqdn}/*" >/dev/stderr
 else
