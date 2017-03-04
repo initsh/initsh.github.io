5d8a2074304ae5da08bde20a8c515b4203dfde73
initsh <k.ogino.biz@gmail.com>
2017-03-04 15:04:07 +0900

    Update check.centos6.sh

--- check.centos6.sh
+++ check.centos6.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if ! egrep --quiet "Cent.* 6|Red.* 6" /etc/*release
 then
-	echo "This program supports only CentOS 6 or RedHat Enterprise Linux 6."
+	echo "This program supports only CentOS 6 or RedHat Enterprise Linux 6." >/dev/stderr
 	exit 1
 fi
 
