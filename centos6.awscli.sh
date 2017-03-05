d03d8a113e6b3f841d34c77f4032464593260ea1
initsh <k.ogino.biz@gmail.com>
2017-03-06 03:19:31 +0900

    Update centos6.awscli.sh

--- centos6.awscli.sh
+++ centos6.awscli.sh
@@ -16,17 +16,23 @@ v_script_name="centos6.awscli.sh"
 	then
 		LogInfo "Run \"get-pip.py\"."
 		python <(curl -LRs "${v_url_getpip}")
-	else
-		LogInfo "\"pip\" already installed."
+		if [ "$(pip --help >/dev/null 2>&1; echo $?)" -eq 127 ]
+		then
+			LogError "Failed to install \"pip\"."
+			exit 1
+		fi
 	fi
 
 	# awscli
 	if [ "$(aws --help >/dev/null 2>&1; echo $?)" -eq 127 ]
 	then
 		LogInfo "bash# pip install awscli"
-		pip install awscli | StdoutLog
-	else
-		LogInfo "\"awscli\" already installed."
+		pip install awscli
+		if [ "$(aws --help >/dev/null 2>&1; echo $?)" -eq 127 ]
+		then
+			LogError "Failed to install \"awscli\"."
+			exit 1
+		fi
 	fi
 
 	LogInfo "End \"${v_script_name}\"."
