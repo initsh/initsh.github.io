36b5c8120172f02b3be07d56782078de8e0f49b2
initsh <k.ogino.biz@gmail.com>
2017-03-06 01:13:14 +0900

    Update centos6.awscli.sh

--- centos6.awscli.sh
+++ centos6.awscli.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
+# Edit 20170306
+v_script_name="centos6.awscli.sh"
+
 # functions
-. <(curl -LRs initsh.github.io/functions.sh)
-echo "[INFO]: Start centos6.awscli.sh" | StdoutLog
+. <(curl -LRs initsh.github.io/functions.sh) || echo "$(date -Is) [ERROR]: Failed to load https://initsh.github.io/functions.sh"
 
-# pip
-if [ "$(pip --help >/dev/null 2>&1; echo $?)" -eq 127 ]
-then
-	echo '[INFO]: python get-pip.py' | StdoutLog
-	python <(curl -LRs "https://bootstrap.pypa.io/get-pip.py") | StdoutLog
-else
-	echo '[INFO]: pip already installed' | StdoutLog
-fi
+{
+	LogInfo "Start \"${v_script_name}\"."
 
-# awscli
-if [ "$(aws --help >/dev/null 2>&1; echo $?)" -eq 127 ]
-then
-	echo '[INFO]: pip install awscli' | StdoutLog
-	pip install awscli | StdoutLog
-else
-	echo '[INFO]: awscli already installed' | StdoutLog
-fi
+	v_url_getpip="https://bootstrap.pypa.io/get-pip.py"
+
+	# pip
+	if [ "$(pip --help >/dev/null 2>&1; echo $?)" -eq 127 ]
+	then
+		LogInfo "Run \"get-pip.py\"."
+		python <(curl -LRs "${v_url_getpip}")
+	else
+		LogInfo "\"pip\" already installed."
+	fi
+
+	# awscli
+	if [ "$(aws --help >/dev/null 2>&1; echo $?)" -eq 127 ]
+	then
+		LogInfo "bash# pip install awscli"
+		pip install awscli | StdoutLog
+	else
+		LogInfo "\"awscli\" already installed."
+	fi
+
+	LogInfo "End \"${v_script_name}\"."
+} >"${v_log_file}"
+
+
+
+
 
 #EOF
