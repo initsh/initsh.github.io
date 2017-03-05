7187e2efb770c1b7c3c1adefc7de54861478353b
initsh <k.ogino.biz@gmail.com>
2017-03-06 01:27:56 +0900

    Update centos7.epel.sh

--- centos7.epel.sh
+++ centos7.epel.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
-. <(curl -LRs initsh.github.io/check.centos7.sh)
-. <(curl -LRs initsh.github.io/check.root.sh)
+# Edit 20170306
+v_script_name="centos7.epel.sh"
 
 # functions
-. <(curl -LRs initsh.github.io/functions.sh)
-echo "[INFO]: Start centos7.epel.sh" | StdoutLog
+. <(curl -LRs initsh.github.io/functions.sh) || echo "$(date -Is) [ERROR]: Failed to load https://initsh.github.io/functions.sh"
 
-# variable
-v_epel_url='https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm'
+{
+	LogInfo "Start \"${v_script_name}\"."
+	
+	# variable
+	v_epel_url='https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm'
+	
+	# install yum-utils
+	if ! rpm -q yum-utils
+	then
+		LogInfo 'bash# yum -y install yum-utils'
+		yum -y install yum-utils
+	fi
+	
+	# install epel-release
+	if ! rpm -q epel-release
+	then
+		LogInfo "bash# yum -y install ${v_epel_url}"
+		yum -y install "${v_epel_url}"
+		LogInfo "bash# yum-config-manager --disable epel*"
+		yum-config-manager --disable epel*
+	fi
+		
+	LogInfo "End \"${v_script_name}\"."
+} >"${v_log_file}"
 
-# install yum-utils
-if ! rpm -q yum-utils | StdoutLog
-then
-	echo '[INFO]: yum -y install yum-utils' | StdoutLog
-	yum -y install yum-utils | StdoutLog
-fi
 
-# install epel-release
-if ! rpm -q epel-release | StdoutLog
-then
-	echo "[INFO]: yum -y install ${v_epel_url}" | StdoutLog
-	yum -y install "${v_epel_url}" 2>/dev/stdout | StdoutLog
-	echo '[INFO]: yum-config-manager --disable epel*' | StdoutLog
-	yum-config-manager --disable epel* | StdoutLog
-fi
-
-#EOF
+# EOF
