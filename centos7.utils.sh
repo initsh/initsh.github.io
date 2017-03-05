34c2edf39bc163a589115222cfbeffab82c39cef
initsh <k.ogino.biz@gmail.com>
2017-03-06 02:08:25 +0900

    Update centos7.utils.sh

--- centos7.utils.sh
+++ centos7.utils.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-curl -LRs "initsh.github.io/centos6.utils.sh" | bash
 
-#EOF
+# Edit 20170306
+v_script_name="centos7.selinux.sh"
+
+# functions
+. <(curl -LRs initsh.github.io/functions.sh) || echo "$(date -Is) [ERROR]: Failed to load https://initsh.github.io/functions.sh"
+
+{
+	LogInfo "Start \"${v_script_name}\"."
+	
+	LogInfo "Redirect."
+	curl -LRs "initsh.github.io/centos6.utils.sh" | bash
+	
+	LogInfo "End \"${v_script_name}\"."
+} >"${v_log_file}"
+
+
+# EOF
