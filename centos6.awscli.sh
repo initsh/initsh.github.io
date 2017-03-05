e312bcd32467da7721815e328cceddec72d4e8a0
initsh <k.ogino.biz@gmail.com>
2017-03-05 19:27:24 +0900

    Update centos6.awscli.sh

--- centos6.awscli.sh
+++ centos6.awscli.sh
@@ -8,7 +8,9 @@ echo "[INFO]: Start centos6.awscli.sh" | StdoutLog
 if [ "$(pip --help >/dev/null 2>&1; echo $?)" -eq 127 ]
 then
 	echo '[INFO]: python get-pip.py' | StdoutLog
-	python <(curl -Ls "https://bootstrap.pypa.io/get-pip.py") | StdoutLog
+	python <(curl -LRs "https://bootstrap.pypa.io/get-pip.py") | StdoutLog
+else
+	echo '[INFO]: pip already installed' | StdoutLog
 fi
 
 # awscli
@@ -16,6 +18,8 @@ if [ "$(aws --help >/dev/null 2>&1; echo $?)" -eq 127 ]
 then
 	echo '[INFO]: pip install awscli' | StdoutLog
 	pip install awscli | StdoutLog
+else
+	echo '[INFO]: awscli already installed' | StdoutLog
 fi
 
 #EOF
