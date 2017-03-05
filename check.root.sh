9474d480fb40e84286639e828a490037e75784df
initsh <k.ogino.biz@gmail.com>
2017-03-06 03:43:55 +0900

    Update check.root.sh

--- check.root.sh
+++ check.root.sh
@@ -4,7 +4,7 @@
 
 if [ "$(whoami)" != "root" ]
 then
-	echo "This program needs to be executed by root user." | tee /dev/stderr
+	echo "$(date -Is) [ERROR]: This program needs to be executed by root user." | tee /dev/stderr
 	exit 1
 fi
 
