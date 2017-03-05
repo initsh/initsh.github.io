d76f4ea55700376ceb6f7ed5bec8fc3f3e8b9715
initsh <k.ogino.biz@gmail.com>
2017-03-06 01:29:26 +0900

    Update centos7.epel.sh

--- centos7.epel.sh
+++ centos7.epel.sh
@@ -10,13 +10,15 @@ v_script_name="centos7.epel.sh"
 	LogInfo "Start \"${v_script_name}\"."
 	
 	# variable
-	v_epel_url='https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm'
+	v_epel_url="https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm"
 	
 	# install yum-utils
 	if ! rpm -q yum-utils
 	then
-		LogInfo 'bash# yum -y install yum-utils'
+		LogInfo "bash# yum -y install yum-utils"
 		yum -y install yum-utils
+	else
+		LogInfo "\"yum-utils\" already installed."
 	fi
 	
 	# install epel-release
@@ -26,6 +28,8 @@ v_script_name="centos7.epel.sh"
 		yum -y install "${v_epel_url}"
 		LogInfo "bash# yum-config-manager --disable epel*"
 		yum-config-manager --disable epel*
+	else
+		LogInfo "\"epel-release\" already installed."
 	fi
 		
 	LogInfo "End \"${v_script_name}\"."
