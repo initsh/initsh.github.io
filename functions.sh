018b36c00dae18367aeba828feb4c41d548de17c
initsh <k.ogino.biz@gmail.com>
2017-03-06 00:28:59 +0900

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# var
+# variables
 v_date="$(date +%Y%m%d)"
 v_time="$(date +%H%M%S)"
 v_backup_suffix="_${v_date}_${v_time}.backup"
 v_log_file="${HOME}/initsh.log"
 
+# functions
 function StdoutLog()
 {
 	cat - | awk '{print "'$(date -Is)' "$0}' >>${HOME}/initsh.log
