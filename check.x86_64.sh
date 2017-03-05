b6b2456a116154998072c0b20aedcf197acb4ac0
initsh <k.ogino.biz@gmail.com>
2017-03-06 03:44:02 +0900

    Update check.x86_64.sh

--- check.x86_64.sh
+++ check.x86_64.sh
@@ -4,7 +4,7 @@
 
 if [ "$(uname -m)" != 'x86_64' ]
 then
-	echo "This program supports only x86_64." | tee /dev/stderr
+	echo "$(date -Is) [ERROR]: This program supports only x86_64." | tee /dev/stderr
 	exit 1
 fi
 
