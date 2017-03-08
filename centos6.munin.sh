754026ce3275aae51202ae5d536f74cb48f20389
initsh <k.ogino.biz@gmail.com>
2017-03-08 20:07:58 +0900

    Update centos6.munin.sh

--- centos6.munin.sh
+++ centos6.munin.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Edit 20170308
-SCRIPT_NAME="centos6.munin.sh"
+v_script_name="centos6.munin.sh"
 
 # functions
 . <(curl -LRs initsh.github.io/functions.sh) || echo "$(date -Is) [ERROR]: Failed to load https://initsh.github.io/functions.sh"
 
 {
-	LogInfo "Start \"${SCRIPT_NAME}\"."
+	LogInfo "Start \"${v_script_name}\"."
 	
 	# checks
 	. <(curl -LRs initsh.github.io/check.centos6.sh)
@@ -17,19 +17,19 @@ SCRIPT_NAME="centos6.munin.sh"
 	. <(curl -LRs initsh.github.io/centos6.epel.sh)
 	
 	# variable
-	MUNIN_USER=admin
-	MUNIN_PASSWORD=munin
+	v_munin_user=admin
+	v_munin_password=munin
 	
 	# install munin
 	yum -y --enablerepo=epel munin
 	
 	# gen passwd
-	echo -e "${MUNIN_PASSWORD}" | htpasswd -i /etc/munin/munin-htpasswd ${MUNIN_USER}
+	echo -e "${v_munin_password}" | htpasswd -i /etc/munin/munin-htpasswd ${v_munin_user}
 	
 	# load /etc/httpd/conf.d/munin.conf
 	service httpd restart
 	
-	LogInfo "End \"${SCRIPT_NAME}\"."
+	LogInfo "End \"${v_script_name}\"."
 } >>"${v_log_file}"
 
 
