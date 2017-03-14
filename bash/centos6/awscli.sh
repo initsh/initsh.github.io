b5ef2d6aac2e3642d88267656248199d55e9f04f
initsh <k.ogino.biz@gmail.com>
2017-03-14 22:42:14 +0900

    Update awscli.sh

--- bash/centos6/awscli.sh
+++ bash/centos6/awscli.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Edit 20170306
-v_github_dir="https://raw.githubusercontent.com/initsh/initsh.github.io/master/bash"
+v_github_dir="raw.githubusercontent.com/initsh/initsh.github.io/master/bash"
 v_script_name="centos6/awscli.sh"
 
 # functions
