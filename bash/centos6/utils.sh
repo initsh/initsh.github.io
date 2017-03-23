d2d841487e1f9143a698f9a2219cfd503b9d0e0b
initsh <k.ogino.biz@gmail.com>
2017-03-23 19:09:14 +0900

    Update utils.sh

--- bash/centos6/utils.sh
+++ bash/centos6/utils.sh
@@ -102,8 +102,9 @@ v_script_name="centos6/utils.sh"
 	# if not installed (return 127), download jq
 	if [ "$(jq --help >/dev/null 2>&1; echo $?)" -eq 127 ]
 	then
-		echo 'bash# curl $url -o /usr/bin/jq'
-		curl -LRs https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -o /usr/bin/jq && chmod 755 /usr/bin/jq 
+		v_jq_url="https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64"
+		echo "bash# curl ${v_jq_url} -o /usr/bin/jq"
+		curl -LRs "${v_jq_url}" -o /usr/bin/jq && chmod 755 /usr/bin/jq 
 		ls -dl /usr/bin/jq*
 		if [ "$(jq --help >/dev/null 2>&1; echo $?)" -eq 127 ]
 		then
