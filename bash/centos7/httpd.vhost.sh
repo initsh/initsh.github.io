9d9564d4542dff8fae9fbb86c9eaeb0e3df45fc5
initsh <k.ogino.biz@gmail.com>
2017-03-23 18:53:25 +0900

    Update httpd.vhost.sh

--- bash/centos7/httpd.vhost.sh
+++ bash/centos7/httpd.vhost.sh
@@ -19,7 +19,7 @@ v_script_name="centos7/httpd.vhost.sh"
 	if ! rpm --quiet -q openssl
 	then
 		LogInfo "bash# yum -y install openssl"
-		sudo yum -y install openssl
+		yum -y install openssl
 		if ! rpm -q openssl
 		then
 			LogError "Failed to install openssl."
@@ -29,7 +29,7 @@ v_script_name="centos7/httpd.vhost.sh"
 	if ! rpm --quiet -q httpd
 	then
 		LogInfo "bash# yum -y install httpd"
-		sudo yum -y install httpd
+		yum -y install httpd
 		if ! rpm -q httpd
 		then
 			LogError "Failed to install httpd."
@@ -39,7 +39,7 @@ v_script_name="centos7/httpd.vhost.sh"
 	if ! rpm --quiet -q mod_ssl
 	then
 		LogInfo "bash# yum -y install mod_ssl"
-		sudo yum -y install mod_ssl
+		yum -y install mod_ssl
 		if ! rpm -q mod_ssl
 		then
 			LogError "Failed to install mod_ssl."
@@ -53,17 +53,17 @@ v_script_name="centos7/httpd.vhost.sh"
 	# edit conf
 	v_httpd_conf="/etc/httpd/conf/httpd.conf"
 	LogInfo "Edit ${v_httpd_conf}"
-	[ -f ${v_httpd_conf} ] || sudo touch ${v_httpd_conf}
-	sudo cp -p "${v_httpd_conf}" "${v_httpd_conf}${v_backup_suffix}"
+	[ -f ${v_httpd_conf} ] || touch ${v_httpd_conf}
+	cp -p "${v_httpd_conf}" "${v_httpd_conf}${v_backup_suffix}"
 	# edit edit edit edit
 	# if no diff, overwrite file.
-	[ "$(sudo diff "${v_httpd_conf}${v_backup_suffix}" "${v_httpd_conf}")" ] || sudo \mv -f "${v_httpd_conf}${v_backup_suffix}" "${v_httpd_conf}"
+	[ "$(diff "${v_httpd_conf}${v_backup_suffix}" "${v_httpd_conf}")" ] || \mv -f "${v_httpd_conf}${v_backup_suffix}" "${v_httpd_conf}"
 	LogInfo "$(ls -dl "${v_httpd_conf}"*)"
 	
 	# mkdir vhost rootdir
 	v_vhost_fqdn_docroot="/var/www/${v_fqdn}"
 	LogInfo "bash# mkdir ${v_vhost_fqdn_docroot}"
-	sudo mkdir -p "${v_vhost_fqdn_docroot}"
+	mkdir -p "${v_vhost_fqdn_docroot}"
 	LogInfo "$(ls -dl "${v_vhost_fqdn_docroot}"*)"
 	[ -d "${v_vhost_fqdn_docroot}" ] || { LogError "${v_fqdn}: Something wrong about DocumentRoot."; exit 1; }
 	
@@ -72,20 +72,20 @@ v_script_name="centos7/httpd.vhost.sh"
 	v_fqdn_csr="/etc/pki/tls/certs/${v_fqdn}.csr"
 	v_fqdn_crt="/etc/pki/tls/certs/${v_fqdn}.crt"
 	LogInfo "Generate SSL Keys."
-	[ -f "${v_fqdn_key}" ] || sudo openssl genrsa 2048 >${v_fqdn_key} 2>/dev/stdout
-	[ -f "${v_fqdn_csr}" ] || sudo openssl req -new -key ${v_fqdn_key} -subj "/C=JP/CN=${v_fqdn}" >${v_fqdn_csr} 2>/dev/stdout
-	[ -f "${v_fqdn_crt}" ] || sudo openssl x509 -days 3650 -req -signkey ${v_fqdn_key} <${v_fqdn_csr} >${v_fqdn_crt} 2>/dev/stdout
-	LogInfo "$(sudo ls -dl "${v_fqdn_key}" "${v_fqdn_csr}" "${v_fqdn_crt}")"
+	[ -f "${v_fqdn_key}" ] || openssl genrsa 2048 >${v_fqdn_key} 2>/dev/stdout
+	[ -f "${v_fqdn_csr}" ] || openssl req -new -key ${v_fqdn_key} -subj "/C=JP/CN=${v_fqdn}" >${v_fqdn_csr} 2>/dev/stdout
+	[ -f "${v_fqdn_crt}" ] || openssl x509 -days 3650 -req -signkey ${v_fqdn_key} <${v_fqdn_csr} >${v_fqdn_crt} 2>/dev/stdout
+	LogInfo "$(ls -dl "${v_fqdn_key}" "${v_fqdn_csr}" "${v_fqdn_crt}")"
 	
 	# edit fqdn conf
 	v_httpd_conf_d_dir="/etc/httpd/conf.d"
 	v_httpd_vhost_fqdn_conf="${v_httpd_conf_d_dir}/httpd-vhost-${v_fqdn}.conf"
 	LogInfo "Edit ${v_httpd_vhost_fqdn_conf}"
 	[ -d "${v_httpd_conf_d_dir}" ] || { LogError "Something wrong about Directory(${v_httpd_conf_d_dir})."; exit 1; }
-	[ -f "${v_httpd_vhost_fqdn_conf}" ] || sudo touch "${v_httpd_vhost_fqdn_conf}"
-	sudo \cp -p "${v_httpd_vhost_fqdn_conf}" "${v_httpd_vhost_fqdn_conf}${v_backup_suffix}"
+	[ -f "${v_httpd_vhost_fqdn_conf}" ] || touch "${v_httpd_vhost_fqdn_conf}"
+	\cp -p "${v_httpd_vhost_fqdn_conf}" "${v_httpd_vhost_fqdn_conf}${v_backup_suffix}"
 	
-	sudo cat <<__EOD__ >"${v_httpd_vhost_fqdn_conf}"
+	cat <<__EOD__ >"${v_httpd_vhost_fqdn_conf}"
 # Edit 20170304
 # https://github.com/initsh/initsh.github.io
 
@@ -118,11 +118,11 @@ NameVirtualhost *:443
 </VirtualHost>
 __EOD__
 	
-	[ "$(sudo diff "${v_httpd_vhost_fqdn_conf}${v_backup_suffix}" "${v_httpd_vhost_fqdn_conf}")" ] || sudo \mv -f "${v_httpd_vhost_fqdn_conf}${v_backup_suffix}" "${v_httpd_vhost_fqdn_conf}"
+	[ "$(diff "${v_httpd_vhost_fqdn_conf}${v_backup_suffix}" "${v_httpd_vhost_fqdn_conf}")" ] || \mv -f "${v_httpd_vhost_fqdn_conf}${v_backup_suffix}" "${v_httpd_vhost_fqdn_conf}"
 	LogInfo "$(ls -dl "${v_httpd_vhost_fqdn_conf}"*)"
 	
 	# check conf error
-	v_judge="$(sudo httpd -S >/dev/null 2>/dev/stdout)"
+	v_judge="$(httpd -S >/dev/null 2>/dev/stdout)"
 	LogInfo "Check conf."
 	if [ "${v_judge}" ]
 	then
