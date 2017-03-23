667b3c0dae15cbba3ab8b1a3512225801af535cc
initsh <k.ogino.biz@gmail.com>
2017-03-23 18:47:52 +0900

    Update selinux.sh

--- bash/centos6/selinux.sh
+++ bash/centos6/selinux.sh
@@ -15,7 +15,7 @@ v_script_name="centos6/selinux.sh"
 	[ "$(getenforce | grep "^Disabled")" ] || setenforce 0
 	\cp -p "${v_selinux_conf}" "${v_selinux_conf}${v_backup_suffix:?}"
 	sed -r -e 's/^[ \t]*(SELINUX=[^d].+)/#\1\n# '"${v_comment}"'\nSELINUX=disabled/g' "${v_selinux_conf}" -i
-	[ "$(sudo diff "${v_selinux_conf}${v_backup_suffix}" "${v_selinux_conf}")" ] || sudo \mv -f "${v_selinux_conf}${v_backup_suffix}" "${v_selinux_conf}"
+	[ "$(diff "${v_selinux_conf}${v_backup_suffix}" "${v_selinux_conf}")" ] || \mv -f "${v_selinux_conf}${v_backup_suffix}" "${v_selinux_conf}"
 	ls -dl "${v_selinux_conf}"*
 	
 	LogInfo "End \"${v_script_name}\"."
