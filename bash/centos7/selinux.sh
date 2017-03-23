da0ced12ab41442656edc24bb913293c76f932f2
initsh <k.ogino.biz@gmail.com>
2017-03-24 08:11:32 +0900

    Update selinux.sh

--- bash/centos7/selinux.sh
+++ bash/centos7/selinux.sh
@@ -11,7 +11,7 @@ v_script_name="centos7/selinux.sh"
 	LogInfo "Start \"${v_script_name}\"."
 	
 	LogInfo "Redirect."
-	sudo curl -LRs "${v_github_dir}/centos6/selinux.sh" | bash
+	curl -LRs "${v_github_dir}/centos6/selinux.sh" | bash
 	
 	LogInfo "End \"${v_script_name}\"."
 } >>"${v_log_file}"
