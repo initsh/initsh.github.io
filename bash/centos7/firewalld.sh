6c2119699aaf45528426992ddd5c6be6a28ee9b8
initsh <k.ogino.biz@gmail.com>
2017-03-23 18:51:53 +0900

    Update firewalld.sh

--- bash/centos7/firewalld.sh
+++ bash/centos7/firewalld.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo firewall-cmd --zone=public --add-service=http --permanent
-sudo firewall-cmd --zone=public --add-service=https --permanent
-sudo firewall-cmd --reload
+firewall-cmd --zone=public --add-service=http --permanent
+firewall-cmd --zone=public --add-service=https --permanent
+firewall-cmd --reload
 
 #EOF
