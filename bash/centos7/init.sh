cad4c588f59edfcb06270dc0a5339bea485a5f33
initsh <k.ogino.biz@gmail.com>
2017-03-24 11:52:49 +0900

    Update init.sh

--- bash/centos7/init.sh
+++ bash/centos7/init.sh
@@ -4,6 +4,21 @@
 v_github_dir="raw.githubusercontent.com/initsh/initsh.github.io/master/bash"
 v_script_name="centos7/init.sh"
 
-. <(curl -LRs "${v_github_dir}/centos7/selinux.sh")
-. <(curl -LRs "${v_github_dir}/centos7/tz.tokyo.sh")
-. <(curl -LRs "${v_github_dir}/centos7/utils.sh")
+. <(curl -LRs "${v_github_dir}/functions.sh") || echo "$(date -Is) [ERROR]: Failed to load https://${v_github_dir}/functions.sh"
+
+{
+    LogInfo "Start \"${v_script_name}\"."
+    
+	# checks
+	. <(curl -LRs "${v_github_dir}/check/centos7.sh")
+	. <(curl -LRs "${v_github_dir}/check/root.sh")
+    
+    # initialize
+    . <(curl -LRs "${v_github_dir}/centos7/selinux.sh")
+    . <(curl -LRs "${v_github_dir}/centos7/tz.tokyo.sh")
+    . <(curl -LRs "${v_github_dir}/centos7/utils.sh")
+    
+	LogInfo "End \"${v_script_name}\"."
+} >>"${v_log_file}"
+
+
