b4befcee7f3efee7f0ce63712f5684fa5593769d
initsh <k.ogino.biz@gmail.com>
2017-03-24 11:49:54 +0900

    Update init.sh

--- bash/centos7/init.sh
+++ bash/centos7/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 . <(curl -LRs "${v_github_dir}/centos7/selinux.sh")
-. <(curl -LRs "${v_github_dir}/centos7/timezone.sh")
+. <(curl -LRs "${v_github_dir}/centos7/tz.tokyo.sh")
 . <(curl -LRs "${v_github_dir}/centos7/utils.sh")
