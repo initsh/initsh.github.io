5cd20ca06b0c95abc38fc2fdde5457d49bf9e0c4
initsh <k.ogino.biz@gmail.com>
2017-03-24 12:26:05 +0900

    Update init.sh

--- bash/centos7/init.sh
+++ bash/centos7/init.sh
@@ -4,6 +4,7 @@
 v_github_dir="raw.githubusercontent.com/initsh/initsh.github.io/master/bash"
 v_script_name="centos7/init.sh"
 
+# functions
 . <(curl -LRs "${v_github_dir}/functions.sh") || echo "$(date -Is) [ERROR]: Failed to load https://${v_github_dir}/functions.sh"
 
 {
