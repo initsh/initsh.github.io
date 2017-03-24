5e0611aaa0b2c7217a47d3466f0ef3acdcdb9e50
initsh <k.ogino.biz@gmail.com>
2017-03-24 11:50:26 +0900

    Update init.sh

--- bash/centos7/init.sh
+++ bash/centos7/init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Edit 20170324
+v_github_dir="raw.githubusercontent.com/initsh/initsh.github.io/master/bash"
+v_script_name="centos7/init.sh"
+
 . <(curl -LRs "${v_github_dir}/centos7/selinux.sh")
 . <(curl -LRs "${v_github_dir}/centos7/tz.tokyo.sh")
 . <(curl -LRs "${v_github_dir}/centos7/utils.sh")
