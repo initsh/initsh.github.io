f572eda10b528d6627a01f838790d1fee3c3eac8
initsh <k.ogino.biz@gmail.com>
2017-03-29 18:46:57 +0900

    Update httpd.vhost.sh

--- bash/centos7/httpd.vhost.sh
+++ bash/centos7/httpd.vhost.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Edit 20170329
+set -eu
 v_github_dir="raw.githubusercontent.com/initsh/initsh.github.io/master/bash"
 v_script_name="centos7/httpd.vhost.sh"
 
 # functions
-. <(curl -LRs "${v_github_dir}/functions.sh") || echo "$(date -Is) [ERROR]: Failed to load https://${v_github_dir}/functions.sh"
+. <(curl -LRs "${v_github_dir}/functions.sh")
 
 {
     LogInfo "Start \"${v_script_name}\"."
@@ -57,7 +58,7 @@ v_script_name="centos7/httpd.vhost.sh"
     cp -p "${v_httpd_conf}" "${v_httpd_conf}${v_backup_suffix}"
     # edit edit edit edit
     # if no diff, overwrite file.
-    [ "$(diff "${v_httpd_conf}${v_backup_suffix}" "${v_httpd_conf}")" ] || \mv -f "${v_httpd_conf}${v_backup_suffix}" "${v_httpd_conf}"
+    if [ -z "$(diff "${v_httpd_conf}${v_backup_suffix}" "${v_httpd_conf}")" ]; then \mv -f "${v_httpd_conf}${v_backup_suffix}" "${v_httpd_conf}"; fi
     LogInfo "$(ls -dl "${v_httpd_conf}"*)"
 
     # mkdir vhost rootdir
@@ -65,24 +66,24 @@ v_script_name="centos7/httpd.vhost.sh"
     LogInfo "bash# mkdir ${v_vhost_fqdn_docroot}"
     mkdir -p "${v_vhost_fqdn_docroot}"
     LogInfo "$(ls -dl "${v_vhost_fqdn_docroot}"*)"
-    [ -d "${v_vhost_fqdn_docroot}" ] || { LogError "${v_fqdn}: Something wrong about DocumentRoot."; exit 1; }
+    if [ ! -d "${v_vhost_fqdn_docroot}" ]; then LogError "${v_fqdn}: Something wrong about DocumentRoot."; exit 1; fi
 
     # gen fqdn crt & key
     v_fqdn_key="/etc/pki/tls/certs/${v_fqdn}.key"
     v_fqdn_csr="/etc/pki/tls/certs/${v_fqdn}.csr"
     v_fqdn_crt="/etc/pki/tls/certs/${v_fqdn}.crt"
     LogInfo "Generate SSL Keys."
-    [ -f "${v_fqdn_key}" ] || openssl genrsa 2048 >${v_fqdn_key} 2>&1
-    [ -f "${v_fqdn_csr}" ] || openssl req -new -key ${v_fqdn_key} -subj "/C=JP/CN=${v_fqdn}" >${v_fqdn_csr} 2>&1
-    [ -f "${v_fqdn_crt}" ] || openssl x509 -days 3650 -req -signkey ${v_fqdn_key} <${v_fqdn_csr} >${v_fqdn_crt} 2>&1
+    if [ ! -f "${v_fqdn_key}" ]; then openssl genrsa 2048 >${v_fqdn_key} 2>&1; fi
+    if [ ! -f "${v_fqdn_csr}" ]; then openssl req -new -key ${v_fqdn_key} -subj "/C=JP/CN=${v_fqdn}" >${v_fqdn_csr} 2>&1; fi
+    if [ ! -f "${v_fqdn_crt}" ]; then openssl x509 -days 3650 -req -signkey ${v_fqdn_key} <${v_fqdn_csr} >${v_fqdn_crt} 2>&1; fi
     LogInfo "$(ls -dl "${v_fqdn_key}" "${v_fqdn_csr}" "${v_fqdn_crt}")"
 
     # edit fqdn conf
     v_httpd_conf_d_dir="/etc/httpd/conf.d"
     v_httpd_vhost_fqdn_conf="${v_httpd_conf_d_dir}/httpd-vhost-${v_fqdn}.conf"
     LogInfo "Edit ${v_httpd_vhost_fqdn_conf}"
-    [ -d "${v_httpd_conf_d_dir}" ] || { LogError "Something wrong about Directory(${v_httpd_conf_d_dir})."; exit 1; }
-    [ -f "${v_httpd_vhost_fqdn_conf}" ] || touch "${v_httpd_vhost_fqdn_conf}"
+    if [ ! -d "${v_httpd_conf_d_dir}" ]; then LogError "Something wrong about Directory(${v_httpd_conf_d_dir})."; exit 1; fi
+    if [ ! -f "${v_httpd_vhost_fqdn_conf}" ]; then touch "${v_httpd_vhost_fqdn_conf}"; fi
     \cp -p "${v_httpd_vhost_fqdn_conf}" "${v_httpd_vhost_fqdn_conf}${v_backup_suffix}"
 
     cat <<__EOD__ >"${v_httpd_vhost_fqdn_conf}"
@@ -118,7 +119,7 @@ NameVirtualhost *:443
 </VirtualHost>
 __EOD__
 
-    [ "$(diff "${v_httpd_vhost_fqdn_conf}${v_backup_suffix}" "${v_httpd_vhost_fqdn_conf}")" ] || \mv -f "${v_httpd_vhost_fqdn_conf}${v_backup_suffix}" "${v_httpd_vhost_fqdn_conf}"
+    if [ -z "$(diff "${v_httpd_vhost_fqdn_conf}${v_backup_suffix}" "${v_httpd_vhost_fqdn_conf}")" ]; then \mv -f "${v_httpd_vhost_fqdn_conf}${v_backup_suffix}" "${v_httpd_vhost_fqdn_conf}"; fi
     LogInfo "$(ls -dl "${v_httpd_vhost_fqdn_conf}"*)"
 
     # check conf error
