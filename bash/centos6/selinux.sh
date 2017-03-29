13445bcf30a13649cc4a3983e4b11023def074af
initsh <k.ogino.biz@gmail.com>
2017-03-30 02:02:03 +0900

    Update selinux.sh

--- bash/centos6/selinux.sh
+++ bash/centos6/selinux.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-# Edit 20170306
+# Edit 20170330
+set -eu
 v_github_dir="raw.githubusercontent.com/initsh/initsh.github.io/master/bash"
 v_script_name="centos6/selinux.sh"
 
 # functions
-. <(curl -LRs "${v_github_dir}/functions.sh") || echo "$(date -Is) [ERROR]: Failed to load https://${v_github_dir}/functions.sh"
+. <(curl -LRs "${v_github_dir}/functions.sh")
 
 {
-	LogInfo "Start \"${v_script_name}\"."
-	
-	v_selinux_conf="/etc/selinux/config"
-	
-	[ "$(getenforce | grep "^Disabled")" ] || setenforce 0
-	\cp -p "${v_selinux_conf}" "${v_selinux_conf}${v_backup_suffix:?}"
-	sed -r -e 's/^[ \t]*(SELINUX=[^d].+)/#\1\n# '"${v_comment}"'\nSELINUX=disabled/g' "${v_selinux_conf}" -i
-	[ "$(diff "${v_selinux_conf}${v_backup_suffix}" "${v_selinux_conf}")" ] || \mv -f "${v_selinux_conf}${v_backup_suffix}" "${v_selinux_conf}"
-	ls -dl "${v_selinux_conf}"*
-	
-	LogInfo "End \"${v_script_name}\"."
+    LogInfo "Start \"${v_script_name}\"."
+    
+    v_selinux_conf="/etc/selinux/config"
+    
+    if [ -z "$(getenforce | grep "^Disabled")" ]; then setenforce 0; fi
+    \cp -p "${v_selinux_conf}" "${v_selinux_conf}${v_backup_suffix:?}"
+    sed -r -e 's/^[ \t]*(SELINUX=[^d].+)/#\1\n# '"${v_comment}"'\nSELINUX=disabled/g' "${v_selinux_conf}" -i
+    if [ -z "$(diff "${v_selinux_conf}${v_backup_suffix}" "${v_selinux_conf}")" ]; then \mv -f "${v_selinux_conf}${v_backup_suffix}" "${v_selinux_conf}"; fi
+    LogInfo "$(ls -dl "${v_selinux_conf}"*)"
+    
+    LogInfo "End \"${v_script_name}\"."
 } >>"${v_log_file}"
 
 # EOF
