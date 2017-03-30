448c6ef64a07fb494ac0c3ba078df6e502bb1ce5
initsh <k.ogino.biz@gmail.com>
2017-03-31 02:26:45 +0900

    Create sudoers.sh

---
+++ bash/centos6/sudoers.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Edit 20170331
+set -eu
+v_github_dir="raw.githubusercontent.com/initsh/initsh.github.io/master/bash"
+v_script_name="centos6/sudoers.sh"
+
+# functions
+. <(curl -LRs "${v_github_dir}/functions.sh")
+
+{
+    LogInfo "Start \"${v_script_name}\"."
+
+    # checks
+    . <(curl -LRs "${v_github_dir}/check/centos7.sh")
+    . <(curl -LRs "${v_github_dir}/check/root.sh")
+    . <(curl -LRs "${v_github_dir}/check/args.sh")
+
+    # check args
+    if [ -z "$(grep "$1" /etc/password)" ]
+    then
+        LogError "\$1 needs Linux local user."
+        exit 1
+    fi
+
+    usermod -G wheel "${USERNAME}"
+    LogNotice "Modify \"${USERNAME}\" group \"wheel\"."
+    
+    v_sudoers_d="/etc/sudoers.d/${USERNAME}"
+    echo -e "# $(date +%Y%m%d) #\n${USERNAME} ALL=(ALL) NOPASSWD: ALL" >"${v_sudoers_d}"
+    LogNotice "Generate sudoers.d file(${v_sudoers_d})."
+
+    LogInfo "End \"${v_script_name}\"."
+} >>"${v_log_file}"
+
+
+# EOF
