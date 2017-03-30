d832c851a8c9075945aaf04e368407d5efd8de15
initsh <k.ogino.biz@gmail.com>
2017-03-31 02:34:05 +0900

    Update openstack.sh

--- bash/centos7/openstack.sh
+++ bash/centos7/openstack.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+# https://www.rdoproject.org/install/quickstart/
+systemctl disable firewalld
+systemctl stop firewalld
+systemctl disable NetworkManager
+systemctl stop NetworkManager
+systemctl enable network
+systemctl start network
+yum install -y centos-release-openstack-ocata
+yum update -y
+yum install -y openstack-packstack
+
 
