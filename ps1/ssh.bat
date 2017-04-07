ae5db7d6c694677286b55e10747c7c42dd28f7cf
initsh <k.ogino.biz@gmail.com>
2017-04-07 17:49:55 +0900

    Create ssh.bat

---
+++ ps1/ssh.bat
@@ -0,0 +1,16 @@
+@echo off
+
+rem ### sshTeraTerm.ps1 実行バッチ
+
+set "SSH_POWERSHELL_URL=https://raw.githubusercontent.com/initsh/initsh.github.io/master/ps1/sshTeraTerm.ps1"
+set "SSH_POWERSHELL_DIR=%USERPROFILE%\GoogleDrive\ps1"
+set "SSH_POWERSHELL=%USERPROFILE%\GoogleDrive\ps1\sshTeraTerm.ps1"
+
+if not exist "%SSH_POWERSHELL_DIR%" mkdir "%SSH_POWERSHELL_DIR%" 1>nul
+if not exist "%SSH_POWERSHELL%" bitsadmin.exe /TRANSFER get_sshTeraTerm "%SSH_POWERSHELL_URL%" "%SSH_POWERSHELL%"
+
+start powershell.exe "%SSH_POWERSHELL%" %*
+
+move "%~dp0%~nx0" "%USERPROFILE%" 1>nul
+
+rem # EOF
