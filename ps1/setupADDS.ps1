ba5f438614c1859046a8d99dfb01a2389d1ea187
initsh <k.ogino.biz@gmail.com>
2017-05-02 01:50:10 +0900

    Update setupADDS.ps1

--- ps1/setupADDS.ps1
+++ ps1/setupADDS.ps1
@@ -4,7 +4,7 @@
 # 
 # - Contents
 #     Setup Active Directory Domain Services and Domain Controller.
-#     C:\> Invoke-WebRequest -Uri initsh.github.io/ps1/setupADDS.ps1
+#     C:\> Invoke-WebRequest -Uri https://initsh.github.io/ps1/setupADDS.ps1
 # 
 # - Revision
 #     2017-05-01 created.
