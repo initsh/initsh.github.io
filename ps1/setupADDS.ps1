e0742fc4f800511b32a6ec2617f02bbd48444d34
initsh <k.ogino.biz@gmail.com>
2017-05-02 09:06:09 +0900

    Update setupADDS.ps1

--- ps1/setupADDS.ps1
+++ ps1/setupADDS.ps1
@@ -7,6 +7,12 @@
 #     To run this script, follow the command below.
 #       C:\> Invoke-RestMethod https://initsh.github.io/ps1/setupADDS.ps1 | powershell.exe -Command -
 # 
+# - Precondition
+#     - Windows Server 2012 R2
+#     - Completed:
+#         Setting Static IP Address
+#         Setting Hostname ( I mean "$env:COMPUTERNAME" )
+# 
 # - Revision
 #     2017-05-01 created.
 #     yyyy-MM-dd modified.
@@ -19,7 +25,6 @@
 ################
 # Settings OS
 $addsAllowTsConnections   = $True
-$addsHostname             = "adds-01"
 # Settings Active Directory Domain Services
 # Initialize Variables for Build-Parameter of Active Directory Domain Services Server
 $addsDomainName           = "report.local"
@@ -65,16 +70,6 @@ if (((Get-WmiObject Win32_TerminalServiceSetting -Namespace root\cimv2\TerminalS
     # Allow Remote Desktop
     Write-Output "$(Get-Date -Format yyyy-MM-ddTHH:mm:sszzz) [INFO]: Allow Remote Desktop." | Tee-Object -Append $logFile
     (Get-WmiObject Win32_TerminalServiceSetting -Namespace root\cimv2\TerminalServices).SetAllowTsConnections(1,1) | Out-Null
-}
-
-# if $currentHostname (above Initialize Variables) not equal $addsHostname (above Settings OS)
-if (-Not($currentHostname -eq $addsHostname))
-{
-    # Change Hostname $currentHostname to $addsHostname and Reboot
-    Write-Output "$(Get-Date -Format yyyy-MM-ddTHH:mm:sszzz) [INFO]: Change Hostname $currentHostname to $addsHostname." | Tee-Object -Append $logFile
-    Write-Output "$(Get-Date -Format yyyy-MM-ddTHH:mm:sszzz) [INFO]: Reboot." | Tee-Object -Append $logFile
-    Rename-Computer -NewName $addsHostname -Force -Restart
-    Stop-Transcript
 }
 
 
