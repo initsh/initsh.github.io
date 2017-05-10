753fd139536913d406c63125e39155800e00508b
initsh <k.ogino.biz@gmail.com>
2017-05-10 11:39:44 +0900

    Update sudo.bat

--- ps1/sudo.bat
+++ ps1/sudo.bat
@@ -5,6 +5,7 @@ rem #     sudo.bat
 rem # 
 rem # - Contents
 rem #     C:\Windows\System32\runas.exe rundll32.exe shell32.dll,#61
+rem #     It means: <kbd>Windows</kbd> + <kbd>R</kbd>
 rem # 
 rem # - Revision
 rem #     2017-05-10 created.
