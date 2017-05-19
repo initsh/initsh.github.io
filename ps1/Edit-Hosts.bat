7ee0ea29ff2e27c4b38ca27c36c643c31e604aeb
initsh <k.ogino.biz@gmail.com>
2017-05-19 18:04:34 +0900

    Update Edit-Hosts.bat

--- ps1/Edit-Hosts.bat
+++ ps1/Edit-Hosts.bat
@@ -7,7 +7,7 @@ rem # - Contents
 rem #     C:\Windows\System32\runas.exe notepad C:\Windows\System32\drivers\etc\hosts
 rem # 
 rem # - Install
-rem #     PS C:\> Invoke-WebRequest -Uri "https://initsh.github.io/ps1/Edit-Hosts.bat" -OutFile "$env:USERPROFILE\Edit-Hosts.bat"
+rem #     powershell.exe -Command "Invoke-WebRequest -Uri "https://initsh.github.io/ps1/Edit-Hosts.bat" -OutFile "$env:USERPROFILE\Edit-Hosts.bat""
 rem #
 rem # - Revision
 rem #     2017-05-10 created.
