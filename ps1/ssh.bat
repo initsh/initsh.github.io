4847c17da41633a3e9efeaae5d6fb4f3a7f85607
initsh <k.ogino.biz@gmail.com>
2017-05-22 16:11:45 +0900

    Update ssh.bat

--- ps1/ssh.bat
+++ ps1/ssh.bat
@@ -8,7 +8,7 @@
 #     <Windows> + <R> => ssh root@192.168.1.100
 #
 # - Install
-#     powershell.exe -Command "Invoke-RestMethod -Uri "https://initsh.github.io/ps1/ssh.bat" | Out-File -Encoding utf8 "$env:USERPROFILE\ssh.bat""
+#     powershell.exe -Command "Invoke-RestMethod -Uri "https://initsh.github.io/ps1/ssh.bat" -OutFile "$env:USERPROFILE\ssh.bat""
 #
 # - Reference
 #     http://pf-j.sakura.ne.jp/program/tips/ps1bat2.htm
